efbe0f1d4282d9efd7ac66e2858d64f3b33ebe8f
nogaasha <sivosha2010@gmail.com>
2016-04-17 20:36:32 +0300

    add model Contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416162558) do
+ActiveRecord::Schema.define(version: 20160417172628) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacies", force: :cascade do |t|
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "email"
+    t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ test/models/contacy_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ContacyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
