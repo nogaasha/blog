4585194459b90a2f220a40cf1a563d76271dc802
nogaasha <sivosha2010@gmail.com>
2016-04-26 22:46:04 +0300

    change acticle controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,14 @@ class ArticlesController < ApplicationController
     end    
   end
   
+  def destroy
+     @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end  
+    
+  
   private
    def article_params
       params.require(:article).permit(:title, :text)    #разрешаем только те поля к-е нам нужны
